3e0f191d0b2342f2c3ea82f77fedd1747618de73
Adrian Unger <dev@staydecent.ca>
2021-03-24 10:10:21 -0700

    Update deps, remove prototype polluting.

---
+++ src/check-arg-types.js
@@ -0,0 +1,47 @@
+export function toType(val) {
+  var str = {}.toString.call(val);
+  return str.toLowerCase().slice(8, -1);
+}
+
+export function checkArgTypes(args, types) {
+  args = Array.prototype.slice.call(args);
+
+  var givenType;
+  var expectedType;
+
+  for (var x = 0, len = args.length; x < len; ++x) {
+    givenType = toType(args[x]);
+    expectedType = types[x];
+    if (expectedType === "-any") {
+      continue;
+    }
+
+    if (toType(expectedType) === "array") {
+      var accepted = false;
+      for (var p = 0, l = expectedType.length; p < l; ++p) {
+        accepted = accepted || givenType === expectedType[p];
+      }
+      if (!accepted) {
+        throw new TypeError(
+          "Expected one of [" +
+            expectedType.join(", ") +
+            "] given '" +
+            givenType +
+            "' for argument at index " +
+            x
+        );
+      }
+    } else {
+      if (givenType !== expectedType) {
+        throw new TypeError(
+          "Expected '" +
+            expectedType +
+            "' given '" +
+            givenType +
+            "' for argument at index " +
+            x
+        );
+      }
+    }
+  }
+}
